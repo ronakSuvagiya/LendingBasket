65424c9f26d0f7e5ff9fb4be06e37becc53a0063
Aakash <Aakash@localhost127.0.0.1>
2018-12-10 10:47:08 +0530

    Creates a New Project

---
+++ LeadingBasket/src/main/java/com/akkidev/springproject/LeadingBasketApplication.java
@@ -0,0 +1,12 @@
+package com.akkidev.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadingBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadingBasketApplication.class, args);
+	}
+}
